483616c50d03b76c3d52bf4221c6bfd47ea968bb
Gregorio Maclen <gregorio.maclen@gmail.com>
2022-12-10 15:46:46 -0300

    add rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  rolify
   has_secure_password
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:client) if self.roles.blank?
+  end
 
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_10_173932) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_10_184015) do
   create_table "branches", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.integer "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource"
   end
 
   create_table "schedules", force: :cascade do |t|
@@ -35,6 +46,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_10_173932) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+  end
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
   add_foreign_key "schedules", "branches"
