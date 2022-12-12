9996acd6e9bcf99e9b0ce2f6a163a26164a4043b
Gregorio Maclen <gregorio.maclen@gmail.com>
2022-12-12 11:04:51 -0300

    add logic to attend turns

--- app/controllers/attend_controller.rb
+++ app/controllers/attend_controller.rb
@@ -13,10 +13,14 @@ class AttendController < ApplicationController
 
   def attend
     @turn = Turn.find(params[:id])
+    if params[:result].blank?
+      redirect_to "/attend", alert: "Debe ingresar un resultado."
+      return
+    end
     if @turn.update(status: "attended", manager_id: @user.id, result: params[:result])
-      redirect_to attend_index_path, notice: "Se ha atendido el turno."
+      redirect_to "/attend", notice: "Se ha atendido el turno."
     else
-      redirect_to attend_index_path, alert: "No se ha podido atender el turno."
+      redirect_to "/attend", alert: "No se ha podido atender el turno."
     end
   end
 end
