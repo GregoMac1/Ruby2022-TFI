2eec21628f6b9901fcc4ed2ca4abfb560e0c85f1
Gregorio Maclen <gregorio.maclen@gmail.com>
2022-12-12 11:10:29 -0300

    fix turn status

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -1,3 +1,14 @@
 class Turn < ApplicationRecord
   validates :client_id, :branch_id, :date, :time, :reason, presence: true
+
+  def get_status_name
+    case status
+    when "pending"
+      "Pendiente"
+    when "attended"
+      "Atendido"
+    when "not_attended"
+      "No atendido"
+    end
+  end
 end
